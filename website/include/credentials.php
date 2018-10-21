af57784fca5dd59f4fd60a38905478f202c087e5
annesa12 <42550457+annesa12@users.noreply.github.com>
2018-10-21 15:44:07 -0400

    credentials.php
    it5236

---
+++ website/include/credentials.php
@@ -0,0 +1,9 @@
+<?php
+class Credentials {
+    // Declare the credentials to the database
+    public $servername = "localhost";
+    public $serverusername = "root";
+    public $serverpassword = "nini5190";
+    public $serverdb = "it5236";
+}
+?>
