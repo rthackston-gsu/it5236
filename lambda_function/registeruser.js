cf08a34245f09a96dd33e776676decfcb8e2b419
Russell Thackston <rthackston@georgiasouthern.edu>
2018-11-15 11:34:35 -0500

    Added 'else' to check for connection success

--- lambda_function/registeruser.js
+++ lambda_function/registeruser.js
@@ -44,57 +44,58 @@ exports.handler = (event, context, callback) => {
 		if (err)  {
 			// This should be a "Internal Server Error" error
 			callback(formatErrorResponse('INTERNAL_SERVER_ERROR', [err]));
-		};
-		console.log("Connected!");
-		var sql = "SELECT COUNT(*) AS codecount FROM registrationcodes WHERE LOWER(registrationcode) = LOWER(?)  ";
-		
-		conn.query(sql, [event.registrationcode], function (err, result) {
-		  	if (err) {
-				// This should be a "Internal Server Error" error
-				callback(formatErrorResponse('INTERNAL_SERVER_ERROR', [err]));
-		  	} else {
-		    	console.log("Registration code count is " + result[0].codecount);
-		    	if (result[0].codecount == 0){
-		    		errors.push("Bad registration code");
-					callback(formatErrorResponse('BAD_REQUEST', errors));
-		    	} else {
-					var sql = "INSERT INTO users (userid, username, passwordhash, email) " +
-		                "VALUES (?, ?, ?, ?)";
-					conn.query(sql, [event.userid, event.username, event.passwordHash, event.email], function (err, result) {
-						if (err) {
-							// Check for duplicate values
-							if(err.errno == 1062) {
-								console.log(err.sqlMessage);
-								if(err.sqlMessage.indexOf('username') != -1) {
-									// This should be a "Internal Server Error" error
-									callback(formatErrorResponse('BAD_REQUEST', ["Username already exists"]));
-								} else if(err.sqlMessage.indexOf('email') != -1) {
-									// This should be a "Internal Server Error" error
-									callback(formatErrorResponse('BAD_REQUEST', ["Email address already registered"]));
+		} else {
+			console.log("Connected!");
+			var sql = "SELECT COUNT(*) AS codecount FROM registrationcodes WHERE LOWER(registrationcode) = LOWER(?)  ";
+			
+			conn.query(sql, [event.registrationcode], function (err, result) {
+			  	if (err) {
+					// This should be a "Internal Server Error" error
+					callback(formatErrorResponse('INTERNAL_SERVER_ERROR', [err]));
+			  	} else {
+			    	console.log("Registration code count is " + result[0].codecount);
+			    	if (result[0].codecount == 0){
+			    		errors.push("Bad registration code");
+						callback(formatErrorResponse('BAD_REQUEST', errors));
+			    	} else {
+						var sql = "INSERT INTO users (userid, username, passwordhash, email) " +
+			                "VALUES (?, ?, ?, ?)";
+						conn.query(sql, [event.userid, event.username, event.passwordHash, event.email], function (err, result) {
+							if (err) {
+								// Check for duplicate values
+								if(err.errno == 1062) {
+									console.log(err.sqlMessage);
+									if(err.sqlMessage.indexOf('username') != -1) {
+										// This should be a "Internal Server Error" error
+										callback(formatErrorResponse('BAD_REQUEST', ["Username already exists"]));
+									} else if(err.sqlMessage.indexOf('email') != -1) {
+										// This should be a "Internal Server Error" error
+										callback(formatErrorResponse('BAD_REQUEST', ["Email address already registered"]));
+									} else {
+										// This should be a "Internal Server Error" error
+										callback(formatErrorResponse('BAD_REQUEST', ["Duplicate value"]));
+									}
 								} else {
 									// This should be a "Internal Server Error" error
-									callback(formatErrorResponse('BAD_REQUEST', ["Duplicate value"]));
+									callback(formatErrorResponse('INTERNAL_SERVER_ERROR', [err]));
 								}
-							} else {
-								// This should be a "Internal Server Error" error
-								callback(formatErrorResponse('INTERNAL_SERVER_ERROR', [err]));
-							}
-			      		} else {
-				      		var sql = "INSERT INTO userregistrations (userid, registrationcode) " +
-		                    "VALUES (?, ?)";
-									conn.query(sql, [event.userid, event.registrationcode], function (err, result) {
-								if (err) {
-					        		callback(formatErrorResponse('INTERNAL_SERVER_ERROR', [err]));
-					      		} else {
-						        	console.log("successful registration");
-					      			callback(null,"user registration successful");
-				      			}
-		      				}); //query userregistrations
-			      		} //error users
-		    			}); //query users
-		  			} //good registration
-				} //good code count
-		  	}); //query registration codes
-		}); //connect database
+				      		} else {
+					      		var sql = "INSERT INTO userregistrations (userid, registrationcode) " +
+			                    "VALUES (?, ?)";
+										conn.query(sql, [event.userid, event.registrationcode], function (err, result) {
+									if (err) {
+						        		callback(formatErrorResponse('INTERNAL_SERVER_ERROR', [err]));
+						      		} else {
+							        	console.log("successful registration");
+						      			callback(null,"user registration successful");
+					      			}
+			      				}); //query userregistrations
+				      		} //error users
+			    			}); //query users
+			  			} //good registration
+					} //good code count
+			  	}); //query registration codes
+			}); //connect database
+		} // no connection errors
 	} //no validation errors
 } //handler
--- lambda_function/userregistrations.js
+++ lambda_function/userregistrations.js
@@ -24,43 +24,44 @@ exports.handler = (event, context, callback) => {
 		callback(formatErrorResponse('BAD_REQUEST', errors));
 	} else {
 	
-	//getConnection equivalent
-	var conn = mysql.createConnection({
-		host 	: config.dbhost,
-		user 	: config.dbuser,
-		password : config.dbpassword,
-		database : config.dbname
-	});
-	
-	//attempts to connect to the database
-	conn.connect(function(err) {
-	  	
-		if (err)  {
-			// This should be a "Internal Server Error" error
-			callback(formatErrorResponse('INTERNAL_SERVER_ERROR', [err]));
-		};
-		console.log("Connected!");
-		var sql = "SELECT registrationcode FROM userregistrations WHERE userid = ?";
+		//getConnection equivalent
+		var conn = mysql.createConnection({
+			host 	: config.dbhost,
+			user 	: config.dbuser,
+			password : config.dbpassword,
+			database : config.dbname
+		});
 		
-		conn.query(sql, [event.userid], function (err, result) {
-		  	if (err) {
+		//attempts to connect to the database
+		conn.connect(function(err) {
+		  	
+			if (err)  {
 				// This should be a "Internal Server Error" error
 				callback(formatErrorResponse('INTERNAL_SERVER_ERROR', [err]));
-		  	} else {
-		  		// Pull out just the codes from the "result" array (index '1')
-		  		var codes = [];
-		  		for(var i=0; i<result.length; i++) {
-					codes.push(result[i]['registrationcode']);
-				}
-				// Build an object for the JSON response with the userid and reg codes
-				var json = { 
-					userid : event.userid,
-					userregistrations : codes
-				};
-				// Return the json object
-      			callback(null, json);
-		  	}
-		  	}); //query registration codes
+			} else {
+				console.log("Connected!");
+				var sql = "SELECT registrationcode FROM userregistrations WHERE userid = ?";
+				
+				conn.query(sql, [event.userid], function (err, result) {
+				  	if (err) {
+						// This should be a "Internal Server Error" error
+						callback(formatErrorResponse('INTERNAL_SERVER_ERROR', [err]));
+				  	} else {
+				  		// Pull out just the codes from the "result" array (index '1')
+				  		var codes = [];
+				  		for(var i=0; i<result.length; i++) {
+							codes.push(result[i]['registrationcode']);
+						}
+						// Build an object for the JSON response with the userid and reg codes
+						var json = { 
+							userid : event.userid,
+							userregistrations : codes
+						};
+						// Return the json object
+		      			callback(null, json);
+				  	}
+				}); //query registration codes
+			} // no connection error
 		}); //connect database
 	} //no validation errors
 } //handler
